a5394745c19aae620cd7f230faca3e274d0b11cf
bharadwajjitam101 <bharadwajjitam@gmail.com>
2025-07-27 20:54:10 +0530

    star projects change

--- src/app/projects/0/page.tsx
+++ src/app/projects/0/page.tsx
@@ -126,7 +126,7 @@ export default function Project0() {
               <iframe
                 width="100%"
                 height="100%"
-                src="https://www.youtube.com/embed/MyvfF1MNbt4?rel=0"
+                src="https://www.youtube.com/embed/ffQotByYehw?si=_jw3Td4g6g_4oNiY"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
